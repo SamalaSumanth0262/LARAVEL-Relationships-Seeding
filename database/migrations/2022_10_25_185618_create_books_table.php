6ac912308cda892cbebb6fe07201a8df49f0381c
Samala Sumanth <ssumanth@roostify.com>
2022-10-26 15:03:25 +0100

    First Good Commi

---
+++ database/migrations/2022_10_25_185618_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->integer('library_id');
+            $table->timestamps();
+            $table->string('name');
+            $table->string('author_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
